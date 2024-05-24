27b0ba37db7b3bdf9a7bc64ac04f8629efe174c0
jovanaradic <jokeradic0710@gmail.com>
2024-05-24 21:01:32 +0200

    sredjen dizajn taba1 i pokusaj button-a izmijeni

---
+++ garnizoni/garnizoni/WindowEdit1.xaml.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace garnizoni
+{
+    /// <summary>
+    /// Interaction logic for WindowEdit1.xaml
+    /// </summary>
+    public partial class WindowEdit1 : Window
+    {
+        private Garnizon garnizon;
+        private ObservableCollection<Garnizon> garnizoni;
+
+        public WindowEdit1(ObservableCollection<Garnizon> Garnizoni, Garnizon g)
+        {
+            InitializeComponent();
+            garnizon = g;
+            garnizoni = Garnizoni;
+
+            tbID.Text = garnizon.Id.ToString();
+            tbNaziv.Text = garnizon.Naziv.ToString();
+            tbAdresa.Text = garnizon.Adresa.ToString();
+            string putanja = garnizon.Putanja.ToString();
+            slikaGarnizona.Source = new BitmapImage(new Uri(@putanja, UriKind.Relative));
+        }
+
+        private void Browse_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "Image files (*.jpg)|*.jpg|All Files (*.*)|*.*";
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.Multiselect = false;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string putanja = openFileDialog.FileName;
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(putanja);
+                bitmap.EndInit();
+                slikaGarnizona.Source = bitmap;
+            }
+        }
+
+        private void Izmijeni_Click(object sender, RoutedEventArgs e)
+        {
+            int id;
+            if (tbID.Text != null && tbNaziv.Text != null && tbAdresa.Text != null && slikaGarnizona.Source != null)
+            {
+                if (Int32.TryParse(tbID.Text, out id))
+                {
+                    foreach (var ga in garnizoni)
+                    {
+                        if (ga.Id == id && id != garnizon.Id)
+                        {
+                            MessageBox.Show("Uneseni ID je trenutno zauzet!", "Greska!", MessageBoxButton.OK, MessageBoxImage.Error);
+                            return;
+                        }
+                    }
+                    garnizon.Id = id;
+                    garnizon.Naziv = tbNaziv.Text;
+                    garnizon.Adresa = tbAdresa.Text;
+                    garnizon.Putanja = (slikaGarnizona.Source as BitmapImage)?.UriSource.AbsolutePath;
+                    garnizoni.Add(garnizon);
+                    MessageBox.Show("Uspjesno dodavanje novog garnizona!", "Uspjesna validacija!", MessageBoxButton.OK, MessageBoxImage.Information);
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Unijeli ste ID u pogresnom formatu!", "Greska!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Morate da popunite sva polja prije dodavanja!", "Greska!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+
+        private void Zatvori_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
